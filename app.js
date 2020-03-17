5b9352e9f45fa85c21efd90c1264e24cc0614f17
dalai <dalai0727@yahoo.com>
2020-03-18 00:06:37 +0800

    yalagchiig todruulah, score deer onoo nemeh

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ function unlucy() {
   document.querySelector(".player-1-panel").classList.toggle("active");
 }
 */
-// soog shideh event litener
+// shoog shideh event litener
 document.querySelector(".btn-roll").addEventListener("click", function() {
   //1-6 hurtel sanamsargui toog gargaj irne
   var diceNumber = Math.floor(Math.random() * 6) + 1;
@@ -34,19 +34,36 @@ document.querySelector(".btn-roll").addEventListener("click", function() {
     // 1 ees yalgaatai too buulaa
     roundScore = roundScore + diceNumber;
     document.getElementById("current-" + activePlayer).textContent = roundScore;
-    document.querySelector(".btn-hold").addEventListener("click", function() {
-      document.getElementById("score-" + activePlayer).textContent = roundScore;
-    });
   } else {
     //1 buusan tul toglogchiin eeljiig solino
-
-    roundScore = 0;
-    diceDom.style.display = "none";
-    document.getElementById("current-" + activePlayer).textContent = 0;
-
-    activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
-
-    document.querySelector(".player-0-panel").classList.toggle("active");
-    document.querySelector(".player-1-panel").classList.toggle("active");
+    switchToNextPlayer();
   }
 });
+// hold tovchnii eventlistener
+
+document.querySelector(".btn-hold").addEventListener("click", function() {
+  scores[activePlayer] = scores[activePlayer] + roundScore;
+  document.getElementById("score-" + activePlayer).textContent =
+    scores[activePlayer];
+  if (scores[activePlayer] >= 20) {
+    document.getElementById("name-" + activePlayer).textContent = "WINNER!!!";
+    document
+      .querySelector(".player-" + activePlayer + "-panel")
+      .classList.add("winner");
+  } else {
+    switchToNextPlayer();
+  }
+});
+
+//......toglogchiin eeljiig solih function........
+function switchToNextPlayer() {
+  roundScore = 0;
+  diceDom.style.display = "none";
+  document.getElementById("current-" + activePlayer).textContent = 0;
+
+  activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+
+  document.querySelector(".player-0-panel").classList.toggle("active");
+  document.querySelector(".player-1-panel").classList.toggle("active");
+  //..................................
+}
