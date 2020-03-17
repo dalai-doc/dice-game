9dc411284e6e844cf2a4181d02f448d2c543de69
dalai <dalai0727@yahoo.com>
2020-03-17 21:59:25 +0800

    shoog shiddeg bolloo

---
+++ app.js
@@ -0,0 +1,18 @@
+var activePlayer = 1;
+var scores = [0, 0];
+var roundScore = 0;
+
+document.getElementById("score-0").textContent = "0";
+document.getElementById("score-1").textContent = "0";
+document.getElementById("current-0").textContent = "0";
+document.getElementById("current-1").textContent = "0";
+
+var diceDom = document.querySelector(".dice");
+diceDom.style.display = "none";
+
+document.querySelector(".btn-roll").addEventListener("click", function() {
+  var diceNumber = Math.floor(Math.random() * 6) + 1;
+
+  diceDom.style.display = "block";
+  diceDom.src = "dice-" + diceNumber + ".png";
+});
