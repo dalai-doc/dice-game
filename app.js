73213fe9c829994bd7d5f454d1eea54b66a66d3f
dalai <dalai0727@yahoo.com>
2020-03-17 23:23:08 +0800

    hereglegciin onoog tooloh eeljiig solih

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var activePlayer = 1;
+var activePlayer = 0;
 var scores = [0, 0];
 var roundScore = 0;
 
@@ -9,10 +9,44 @@ document.getElementById("current-1").textContent = "0";
 
 var diceDom = document.querySelector(".dice");
 diceDom.style.display = "none";
+/*
+function unlucy() {
+  roundScore = 0;
+  diceDom.style.display = "none";
+  document.getElementById("current-" + activePlayer).textContent = 0;
 
+  activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+
+  document.querySelector(".player-0-panel").classList.toggle("active");
+  document.querySelector(".player-1-panel").classList.toggle("active");
+}
+*/
+// soog shideh event litener
 document.querySelector(".btn-roll").addEventListener("click", function() {
+  //1-6 hurtel sanamsargui toog gargaj irne
   var diceNumber = Math.floor(Math.random() * 6) + 1;
-
+  // shoonii zurgiig gargaj irne
   diceDom.style.display = "block";
+  //buusan sanamsargui toond taarsan zurgiig gargaj irne
   diceDom.src = "dice-" + diceNumber + ".png";
+  //buusan toon 1 ees yalgaaatai bol idvehitei toglogchiin toog nemegduulne
+  if (diceNumber !== 1) {
+    // 1 ees yalgaatai too buulaa
+    roundScore = roundScore + diceNumber;
+    document.getElementById("current-" + activePlayer).textContent = roundScore;
+    document.querySelector(".btn-hold").addEventListener("click", function() {
+      document.getElementById("score-" + activePlayer).textContent = roundScore;
+    });
+  } else {
+    //1 buusan tul toglogchiin eeljiig solino
+
+    roundScore = 0;
+    diceDom.style.display = "none";
+    document.getElementById("current-" + activePlayer).textContent = 0;
+
+    activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+
+    document.querySelector(".player-0-panel").classList.toggle("active");
+    document.querySelector(".player-1-panel").classList.toggle("active");
+  }
 });
